1930fda704219b9a6b09c987d5f85dc5effee200
교사김도연 <kdyn1717@junghwa.sen.ms.kr>
2026-05-05 21:57:18 +0900

    Add Firebase config

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBxTjbDwAxd--UEZle_xd0f56p6dThnVSs",
+  authDomain: "morning-quest.firebaseapp.com",
+  databaseURL: "https://morning-quest-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "morning-quest",
+  storageBucket: "morning-quest.firebasestorage.app",
+  messagingSenderId: "667349003991",
+  appId: "1:667349003991:web:1901d9f0ab29afd6ab0a92",
+  measurementId: "G-PX1HC9FY48",
+};
